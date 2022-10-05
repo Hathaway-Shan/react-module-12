166d7e51247a97c8a1a43a483ea4f8a0f8488cce
Hathaway-Shan <hathaway.shan@gmail.com>
2022-10-04 17:58:49 -0700

    deleting unused variables for deploy

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,5 +1,5 @@
 // import { useContext } from 'react';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState } from 'react';
 
 const GameContext = createContext();
 
